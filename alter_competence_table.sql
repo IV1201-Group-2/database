a5b71de1ddda5a6ef266e97a5a36b5e086825043
Azmeer K. <azmeer.khawaja@outlook.com>
2024-02-18 22:50:01 +0100

    Add statements to alter existing table to use i18n keys.

---
+++ alter_competence_table.sql
@@ -0,0 +1,12 @@
+-- Rename column 'name' to 'i18n_key'
+ALTER TABLE public.competence RENAME COLUMN name TO i18n_key;
+
+-- Update entries in 'i18n_key' column
+UPDATE public.competence
+SET i18n_key =
+    CASE competence_id
+        WHEN 1 THEN 'ticket-sales'
+        WHEN 2 THEN 'lotteries'
+        WHEN 3 THEN 'roller-coaster-operations'
+        ELSE i18n_key
+    END;
